34cf9a1f21c5f3c988fde6cf9bd51fb7436502f8
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-12-01 18:01:26 +0700

    update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
-/// <reference types="phaser/types/SpineFile.d.ts" />
-/// <reference types="phaser/types/SpineGameObject.d.ts" />
-/// <reference types="phaser/types/SpinePlugin.d.ts" />
+/// <reference types="../node_modules/phaser/types/SpineFile.d.ts" />
+/// <reference types="../node_modules/phaser/types/SpineGameObject.d.ts" />
+/// <reference types="../node_modules/phaser/types/SpinePlugin.d.ts" />
 
 // https://www.typescriptlang.org/docs/handbook/declaration-merging.html
 declare interface Window {
