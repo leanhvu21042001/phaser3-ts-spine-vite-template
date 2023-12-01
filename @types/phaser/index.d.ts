b8accdc013f5c0eb04712e2dd4d10d098101b56c
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-12-01 18:16:07 +0700

    update tsconfig.json

--- @types/phaser/index.d.ts
+++
@@ -1,3 +0,0 @@
-//// <reference path="../../node_modules/phaser/types/SpineFile.d.ts" />
-/// <reference path="../../node_modules/phaser/types/SpineGameObject.d.ts" />
-/// <reference path="../../node_modules/phaser/types/SpinePlugin.d.ts" />
