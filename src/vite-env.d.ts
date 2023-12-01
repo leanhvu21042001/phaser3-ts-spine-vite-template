51b807dcd6d0ab3428456dd79982b6fcc9f8d3ae
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-12-01 18:08:27 +0700

    add @types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="vite/client" />
 
-/// <reference types="../node_modules/phaser/types/SpineFile.d.ts" />
-/// <reference types="../node_modules/phaser/types/SpineGameObject.d.ts" />
-/// <reference types="../node_modules/phaser/types/SpinePlugin.d.ts" />
-
 // https://www.typescriptlang.org/docs/handbook/declaration-merging.html
 declare interface Window {
   SpinePlugin: any;
